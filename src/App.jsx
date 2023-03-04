b335d35aaa7c735ae55a881d7ebd55950248cd3f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-03-04 09:22:26 -0300

    added Button and Count component, refactored onClick functions and added style from Bulma CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,39 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
+import Count from './components/Count'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const decrease = () => {
+    setCount((count) => count - 1)
+  }
+
+  const reset = () => {
+    if (count != 0) {
+      setCount(0)
+    }
+  }
+
+  const increase = () => {
+    setCount((count) => count + 1)
+  }
+
   return (
-    <div className="container">
-      <div className="child">
-        <div className="title">Counter</div>
-        <div
-          className="count"
-          style={{
-            color: count > 0 ? 'green' : '#222222',
-          }}
-        >
-          {count}
-        </div>
-        <div className="buttons">
-          <button
-            id="decrease"
-            onClick={() => {
-              if (count > 0) {
-                setCount((count) => count - 1)
-              }
-            }}
-          >
-            INCREASE
-          </button>
-          <button
-            id="reset"
-            onClick={() => {
-              if (count != 0) {
-                setCount(0)
-              }
-            }}
-          >
-            RESET
-          </button>
-          <button
-            id="increase"
-            onClick={() => {
-              setCount((count) => count + 1)
-            }}
-          >
-            INCREASE
-          </button>
+    <section className="section">
+      <div className="container is-flex is-justify-content-center is-align-content-center is-align-items-center is-align-self-center">
+        <div className="box">
+          <h1 className="title is-1 has-text-centered">Counter</h1>
+          <Count value={count} />
+          <div className="buttons is-flex is-justify-content-center is-align-content-center is-align-items-center is-align-self-center">
+            <Button text="Decrease" handleClick={decrease} />
+            <Button text="Reset" handleClick={reset} />
+            <Button text="Increase" handleClick={increase} />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
